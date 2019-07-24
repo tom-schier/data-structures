fef65ce1757e2cd3e70334e0f7eb1da24ec3636e
tom-schier <schiertxs@gmail.com>
2019-07-25 08:42:16 +1000

    Created new utility class

--- BubbleSort/BubbleSort.cs
+++ BubbleSort/BubbleSort.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Diagnostics;
 using System.Linq;
+using UtitlityStuff;
 
 namespace BubbleSort
 {
@@ -11,7 +12,7 @@ namespace BubbleSort
         {
             Console.WriteLine("BUBBLE SORT");
             int[] arr;
-            InitializeArray(out arr);
+            Utility.InitializeArray(out arr);
 
             //This can be enabled for degugging purposes
             //Console.WriteLine("Input array:");
@@ -31,7 +32,7 @@ namespace BubbleSort
             //Console.WriteLine();
             //WriteArrayToConsole(arr);
 
-            WritePerformance(stopWatch.Elapsed, stopWatch.ElapsedTicks);
+            Utility.WritePerformance(stopWatch.Elapsed, stopWatch.ElapsedTicks);
 
             Console.Write("Enter any key to close ....");
             Console.ReadKey();
@@ -47,80 +48,10 @@ namespace BubbleSort
                 {
                     if (arr[j] < arr[j - 1])
                     {
-                        Swap(ref arr[j], ref arr[j - 1]);
+                        Utility.Swap(ref arr[j], ref arr[j - 1]);
                     }
                 }
             }
-        }
-
-        /*************Utility stuff ***********************/
-
-        private static void InitializeArray(out int[] arr)
-        {
-            bool isInitialised = false;
-            do
-            {
-                Console.WriteLine("Enter array size then ENTER: ");
-                var st = Console.ReadLine();
-                Console.WriteLine("Initialising array....");
-                if (st.Length > 0)
-                {
-                    int cnt;
-                    if (Int32.TryParse(st, out cnt) == true)
-                    {
-
-                        int Min = 0;
-                        int Max = 100000;
-                        Random randNum = new Random();
-                        arr = Enumerable
-                            .Repeat(0, cnt)
-                            .Select(i => randNum.Next(Min, Max))
-                            .ToArray();
-                        isInitialised = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid input argument");
-                        Console.ReadKey();
-                        arr = null;
-                        continue;
-                    };
-
-                }
-                else
-                {
-                    arr = new int[]{
-                        1, 5, 4, 11, 20, 8, 2, 98, 90, 16
-                    };
-                    isInitialised = true;
-                }
-            } while (isInitialised == false);
-
-        }
-
-        private static void Swap(ref int x, ref int y)
-        {
-            int temp = x;
-            x = y;
-            y = temp;
-        }
-
-        private static void WriteArrayToConsole(int[] arr)
-        {
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.Write(arr[i].ToString());
-                if (i < arr.Length - 1)
-                    Console.Write(",");
-            }
-            Console.WriteLine();              
-        }
-
-        private static void WritePerformance(TimeSpan ts, long elapsedTicks)
-        {
-            Console.WriteLine($"Total seconds: {ts.TotalSeconds}");
-            Console.WriteLine($"Total milliseconds: {ts.TotalMilliseconds}");
-            Console.WriteLine("ELAPSED TICKS " + elapsedTicks);
         }
     }
 
---
+++ UtitlityStuff/Utility.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+
+namespace UtitlityStuff
+{
+    public static class Utility
+    {
+        public static void InitializeArray(out int[] arr)
+        {
+            bool isInitialised = false;
+            do
+            {
+                Console.WriteLine("Enter array size then ENTER: ");
+                var st = Console.ReadLine();
+                Console.WriteLine("Initialising array....");
+                if (st.Length > 0)
+                {
+                    int cnt;
+                    if (Int32.TryParse(st, out cnt) == true)
+                    {
+
+                        int Min = 0;
+                        int Max = 100000;
+                        Random randNum = new Random();
+                        arr = Enumerable
+                            .Repeat(0, cnt)
+                            .Select(i => randNum.Next(Min, Max))
+                            .ToArray();
+                        isInitialised = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input argument");
+                        Console.ReadKey();
+                        arr = null;
+                        continue;
+                    };
+
+                }
+                else
+                {
+                    arr = new int[]{
+                        1, 5, 4, 11, 20, 8, 2, 98, 90, 16
+                    };
+                    isInitialised = true;
+                }
+            } while (isInitialised == false);
+
+        }
+
+        public static void Swap(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public static void WriteArrayToConsole(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i].ToString());
+                if (i < arr.Length - 1)
+                    Console.Write(",");
+            }
+            Console.WriteLine();
+        }
+
+        public static void WritePerformance(TimeSpan ts, long elapsedTicks)
+        {
+            Console.WriteLine($"Total seconds: {ts.TotalSeconds}");
+            Console.WriteLine($"Total milliseconds: {ts.TotalMilliseconds}");
+            Console.WriteLine("ELAPSED TICKS " + elapsedTicks);
+        }
+    }
+}
